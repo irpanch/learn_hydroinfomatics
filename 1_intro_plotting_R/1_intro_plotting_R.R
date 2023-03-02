1c99a137501500731d1c9e8b71c439b76f2d4e24
irpanch <irpanch@gmail.com>
2023-03-02 09:34:31 +0700

    update 2.12 (finish chapter 2)

--- 1_intro_plotting_R/1_intro_plotting_R.R
+++ 1_intro_plotting_R/1_intro_plotting_R.R
@@ -34,7 +34,19 @@ ggplot(data=SNP, aes(x=datetime, y=cfs))+
   geom_line()+
   facet_grid(StationID ~ month,scales = "free")
 
-# 
+# 2.10 Box plots
+ggplot(data=SNP, aes(x=StationID, y=cfs))+
+  stat_boxplot()+
+  scale_y_log10()
+
+# 2.11 More about color, size, etc
+ggplot(RBI, aes(RBI, DRAIN_SQKM, size=T_AVG_SITE, bg=STATE))+
+  geom_point(pch=21, alpha=0.3)
+
+# 2.12 Multiple geoms
+ggplot(RBI, aes(RBI, DRAIN_SQKM, color=AGGECOREGION))+
+  stat_smooth(method = "lm", linetype=2)+
+  geom_point()
 
 
              
