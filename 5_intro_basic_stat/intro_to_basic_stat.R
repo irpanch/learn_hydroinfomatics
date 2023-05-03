dba65d3fbe86872402e385357ccf53ab87b32ec5
irpanch <irpanch@gmail.com>
2023-05-04 06:38:00 +0700

    until chapter 5.4

--- 5_intro_basic_stat/intro_to_basic_stat.R
+++ 5_intro_basic_stat/intro_to_basic_stat.R
@@ -5,6 +5,23 @@ library(patchwork)
 
 theme_set(theme_classic())
 
+# some question
+
+## what is difference between sample and population
+### sample is part of population.
+### population is the thing we want to calculate, sample is data 
+### measured in population. sample is subset of population
+
+
+## how do we look at the distribution of data in sample?
+### 
+###
+###
+###
+
+
+
+
 #generate a normal distribution
 ExNorm <- rnorm(1000, mean = 5) %>% 
   as_tibble()
@@ -33,11 +50,53 @@ expdf <- ExNorm %>%
   stat_density()
 
 #put the plots side by side with + or on top of each other with /
-exhist/expdf # ini error
+exhist/expdf
 
-# ini coba pakai cara lain, karena yang dibuku error.
+# create a population of calcium concentrate
+all_the_water <- rnorm(10000, mean=6) %>% as_tibble()
 
-# install.packages("gridExtra")  #ini pertama aja.           
-library("gridExtra")     
+sample_of_water <- sample(all_the_water$value, size = 100,
+                          replace = FALSE) %>% as_tibble()
 
-grid.arrange(exhist, expdf, nrow = 2)  
+population_hist <- all_the_water %>% 
+  ggplot(aes(value))+
+  geom_histogram()+
+  ggtitle("Population: All the water in the lake")
+
+sample_hist <- sample_of_water %>% 
+  ggplot(aes(value))+
+  geom_histogram()+
+  ggtitle("Your sample of the lake")
+
+population_hist+sample_hist 
+
+# measure central tendency
+## goals: finding the middle value (mean, meadian, weighted mean),
+## is it skew or not?
+## how variable is it
+
+pineQ <- read_csv("1_intro_plotting_R/PINE_Jan-Mar_2010.csv")
+
+# calculate mean and median cfs in pineQ and values in ExNorm
+pineMean <- mean(pineQ$cfs)
+pineMedian <- median(pineQ$cfs)
+
+xmean <- mean(ExNorm$value)
+xmedian <- mean(ExNorm$value)
+
+# plot mean and median
+Ex <- ExNorm %>% ggplot(aes(value))+
+  geom_histogram()+
+  geom_vline(xintercept = xmean,color="red")+
+  geom_vline(xintercept = xmedian,color="blue") # mean&median same because normal dist.
+
+PineP <- pineQ %>% ggplot(aes(cfs))+
+  geom_histogram()+
+  geom_vline(xintercept = pineMean,color="red")+
+  geom_vline(xintercept = pineMedian,color="blue")
+
+
+Ex / PineP
+
+
+
