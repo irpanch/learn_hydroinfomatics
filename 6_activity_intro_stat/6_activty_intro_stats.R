63e2cea0a62f6c3e19052fa82d16879f21c530f2
irpanch <irpanch@gmail.com>
2023-06-03 06:24:59 +0700

    finished untile problem 6.6

--- 6_activity_intro_stat/6_activty_intro_stats.R
+++ 6_activity_intro_stat/6_activty_intro_stats.R
@@ -80,3 +80,37 @@ PineN_2 <- PineN + geom_text(aes(pineN_mean,400,label=pineN_mean, hjust=-0.1, vj
   geom_text(aes(pineN_median,400,label=pineN_median, hjust=1, vjust=0))
 
 PineP_2 / PineN_2
+
+
+## 6.5 Problem 5
+### measure the variable 
+
+### variance and standard deviation. sensitive to outliers.
+var(pineQ$cfs)
+var(pine_nfdr$cfs)
+
+sd(pineQ$cfs)
+sd(pine_nfdr$cfs)
+
+### coefficient of variance
+sd(pineQ$cfs)/mean(pineQ$cfs)
+sd(pine_nfdr$cfs)/mean(pine_nfdr$cfs)
+
+### IQR. resistance to outlier
+IQR(pineQ$cfs)
+IQR(pine_nfdr$cfs)
+
+## 6.6 Problem 6
+### compute 3 measure of spread and 2 measure of central tendency
+### sd, cv and IQR. mean and median
+
+pine_nfdr %>% group_by(StationID) %>% 
+  summarize(max_cfs=max(cfs),
+            mean_cfs=mean(cfs),
+            median_cfs=median(cfs),
+            var_cfs=var(cfs),
+            sd_cfs=sd(cfs),
+            IQR_cfs=IQR(cfs))
+### The Pine watershed has more flow (bigger in maximum) also more variable 
+### discharge, we can see it on variance, standard deviation, and IQR is 
+### more bigger than NFDR watershed
